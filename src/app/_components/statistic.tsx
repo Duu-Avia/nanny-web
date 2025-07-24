851c416918a7a7add4de7d22a5f817f10617d09c
Duu-Avia <duuavia01@gmail.com>
2025-07-25 02:05:59 +0800

    commit for deploy

--- src/app/_components/statistic.tsx
+++ src/app/_components/statistic.tsx
@@ -13,21 +13,17 @@ const Statistic = () => {
   </h2>
 
   <div className="max-w-6xl mx-auto flex flex-col md:flex-row justify-center items-center text-[#0f172a] text-center divide-y md:divide-y-0 md:divide-x divide-[#f3c363]">
-    
-    {/* 1. Agencies */}
-    <div className="flex-1 px-6 py-10">
+      <div className="flex-1 px-6 py-10">
       <p className="text-5xl font-bold">7</p>
       <p className="italic mt-2">Agencies in the world</p>
     </div>
 
-    {/* 2. Nannies */}
     <div className="flex-1 px-6 py-10">
       <p className="text-sm uppercase text-gray-500 mb-1">More than</p>
       <p className="text-5xl font-bold">50.000</p>
       <p className="italic mt-2">Nannies registered</p>
     </div>
 
-    {/* 3. Users */}
     <div className="flex-1 px-6 py-10">
       <p className="text-sm uppercase text-gray-500 mb-1">More than</p>
       <p className="text-5xl font-bold">3.000.000</p>
